e853376e3b163de377e00ace7fd27116f180485e
Burak Yilmaz <by@Buraks-MacBook-Pro.local>
2017-12-13 13:21:25 +0300

    supported barcodes

--- app/containers/BarcodeScan.js
+++ app/containers/BarcodeScan.js
@@ -133,7 +133,7 @@ class BarcodeScan extends AppBaseContainer {
                 <Barcode style={{flex: 1, backgroundColor: "transparent"}}
                      ref={ component => this._barCode = component }
                      scannerRectWidth={300}
-                     barCodeTypes={[]}
+                     barCodeTypes={["AZTEC", "CODABAR", "CODE_39", "CODE_93", "CODE_128", "DATA_MATRIX", "EAN_8", "EAN_13", "ITF", "MAXICODE", "PDF_417", "QR_CODE", "RSS_14", "RSS_EXPANDED", "UPC_A", "UPC_E", "UPC_EAN_EXTENSION"]}
                      scannerRectCornerColor="#42f4c5"
                      onBarCodeRead={(data) => this._onBarCodeRead(data)}/>
             </View>
